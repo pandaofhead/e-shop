08763704a7632123727c9971381e9b59d0fc84ea
Hongjin Quan <hq48@cornell.edu>
2024-05-04 10:06:46 -0400

    add Vercel SpeedInsights.

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -1,19 +1,22 @@
 /* eslint-disable react/prop-types */
-import { Analytics } from "@vercel/analytics/react"
+import { Analytics } from "@vercel/analytics/react";
+
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import Footer from "../footer/Footer";
 import Navbar from "../navbar/Navbar";
 
-const Layout = ({children}) => {
-    return (
-        <div>
-            <Navbar/>
-            <div className="main-content min-h-screen">
-                {children}
-                <Analytics />
-            </div>
-            <Footer/>
-        </div>
-    );
-}
+const Layout = ({ children }) => {
+  return (
+    <div>
+      <Navbar />
+      <div className="main-content min-h-screen">
+        {children}
+        <Analytics />
+        <SpeedInsights />
+      </div>
+      <Footer />
+    </div>
+  );
+};
 
 export default Layout;
